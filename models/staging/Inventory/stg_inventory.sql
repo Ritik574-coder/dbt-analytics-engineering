864e1448a96a8408e81dbb9113e1458e999a9df1
ritik574-coder <ritik74820@gmail.com>
2026-06-19 23:01:07 +0530

    adding and edating staging models and writing documentition

--- models/staging/Inventory/stg_inventory.sql
+++ models/staging/Inventory/stg_inventory.sql
@@ -1,16 +1,23 @@
-SELECT  
-     snapshot_date
-    ,product_id
-    ,product_name
-    ,sku
-    ,category
-    ,stock_on_hand
-    ,stock_reserved
-    ,stock_available
-    ,reorder_level
-    ,unit_cost
-    ,unit_price
-    ,inventory_value
-    ,warehouse_location
-    ,store_id
-FROM {{ source('bronze', 'inventory_snapshots') }} ;
+SELECT
+    {{ dbt_utils.generate_surrogate_key([
+        'snapshot_date',
+        'product_id',
+        'store_id'
+    ]) }} AS inventory_snapshot_sk,
+
+    snapshot_date,
+    product_id,
+    product_name,
+    sku,
+    category,
+    stock_on_hand,
+    stock_reserved,
+    stock_available,
+    reorder_level,
+    unit_cost,
+    unit_price,
+    inventory_value,
+    warehouse_location,
+    store_id
+
+FROM {{ source('bronze', 'inventory_snapshots') }};
