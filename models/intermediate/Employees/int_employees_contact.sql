03c9e53e2378011b49cfd6cf6386a7db659bce40
ritik574-coder <ritik74820@gmail.com>
2026-06-22 20:32:03 +0530

    update: adding models and description apply test on the models

--- models/intermediate/Employees/int_employees_contact.sql
+++ models/intermediate/Employees/int_employees_contact.sql
@@ -1,5 +1,21 @@
 SELECT 
     employee_id,
-    email,
-    phone
+
+    CASE 
+        WHEN email IS NULL OR TRIM(email) = '' THEN 'Unknown'
+        WHEN email NOT LIKE '%@%' THEN 'Unknown'
+        WHEN PATINDEX('%@%@%', TRIM(LOWER(email))) > 0 THEN 
+        LEFT(TRIM(LOWER(email)), CHARINDEX('@', TRIM(LOWER(email))) -1)
+        + '@' + REPLACE(SUBSTRING(TRIM(LOWER(email)), CHARINDEX('@', TRIM(LOWER(email))) +1,
+        LEN(TRIM(LOWER(email)))), '@' ,'')
+        ELSE TRIM(LOWER(email))
+    END as email,
+
+    CASE 
+        WHEN phone LIKE '+___________'   THEN  CONCAT('+1 (', SUBSTRING(phone, 3, 3), ') ' ,  SUBSTRING(phone, 6, 3), '-', SUBSTRING(phone, 9,4))
+        WHEN phone LIKE '___.___.____'   THEN  CONCAT('+1 (', SUBSTRING(phone, 1,3) , ') ' ,  SUBSTRING(phone,5, 3), '-' ,  SUBSTRING(phone,9,4))
+        WHEN phone LIKE '__________'     THEN  CONCAT('+1 (', SUBSTRING(phone, 1,3) , ') ' ,  SUBSTRING(phone, 4,3), '-' ,  SUBSTRING(phone,7,4))
+        WHEN phone LIKE '___-___-____'   THEN  CONCAT('+1 (', SUBSTRING(phone,1, 3) , ') ' ,  SUBSTRING(phone, 5,8))
+        WHEN phone LIKE '(___) ___-____' THEN  CONCAT('+1 ',  SUBSTRING(phone, 1,14))
+    END as phone
 FROM {{ ref('stg_employees') }} ;
