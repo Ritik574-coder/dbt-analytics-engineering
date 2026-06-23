d7bd293285fac542a6c309f5cc659dff0c17a5e6
ritik574-coder <ritik74820@gmail.com>
2026-06-23 18:11:26 +0530

    feat(dbt): add intermediate reviews transformation models

--- models/intermediate/Reviews/int_review_feedback.sql
+++ models/intermediate/Reviews/int_review_feedback.sql
@@ -1,6 +1,19 @@
 SELECT
     review_id,
+    
     helpful_votes,
-    review_channel,
-    review_title
+
+    CASE 
+        WHEN TRIM(LOWER(review_channel)) IN ('app', 'mobile app', 'mobile')   THEN 'Mobile App'
+        WHEN TRIM(LOWER(review_channel)) IN ('in store', 'in-store', 'store') THEN 'In Store'
+        WHEN TRIM(LOWER(review_channel)) IN ('online', 'web')                 THEN 'Online'
+        WHEN TRIM(LOWER(review_channel)) = 'phone'                            THEN 'Phone Call'
+        WHEN TRIM(LOWER(review_channel)) = 'catalog'                          THEN 'Catalog'
+        ELSE 'Unknown'
+    END AS review_channel,
+
+    CASE
+        WHEN REPLACE(REPLACE(TRIM(dbo.TitleCase(review_title)), CHAR(13), ''), CHAR(10), '') = '' THEN 'Unknown'
+        ELSE REPLACE(REPLACE(TRIM(dbo.TitleCase(review_title)), CHAR(13), ''), CHAR(10), '')
+    END as review_title
 FROM {{ ref('stg_reviews') }} ;
