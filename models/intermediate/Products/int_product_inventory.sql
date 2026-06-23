e247ce974384b9c8718df91736c6e8b27e9c6f07
ritik574-coder <ritik74820@gmail.com>
2026-06-23 14:18:52 +0530

    feat(dbt): add intermediate product transformation models

--- models/intermediate/Products/int_product_inventory.sql
+++ models/intermediate/Products/int_product_inventory.sql
@@ -1,6 +1,21 @@
 SELECT 
     product_id,
-    is_available,
-    stock_quantity,
-    reorder_level
+    
+    CASE
+        WHEN LOWER(TRIM(is_available)) IN ('a','y','ye','1','t','tr','in','i') THEN 'Available'
+        WHEN LOWER(TRIM(is_available)) IN ('n','no','o','ou') THEN 'Not Available'
+        WHEN LOWER(TRIM(is_available)) IN ('d','di') THEN 'Discontinued'
+        ELSE 'Unknown'
+    END AS is_available,
+
+    CASE 
+        WHEN stock_quantity IS NULL OR TRY_CONVERT(INT , stock_quantity) IS NULL OR TRY_CONVERT(INT , stock_quantity) < 0 THEN NULL
+        ELSE TRY_CONVERT(INT , stock_quantity)
+    END AS stock_quantity,
+
+    CASE 
+        WHEN reorder_level IS NULL OR TRY_CONVERT(INT , reorder_level) IS NULL OR TRY_CONVERT(INT , reorder_level) < 0 THEN NULL
+        ELSE TRY_CONVERT(INT , reorder_level)
+    END AS reorder_level
 FROM {{ ref('stg_products') }} ;
+
--- models/intermediate/Products/int_product_pricing.sql
+++ models/intermediate/Products/int_product_pricing.sql
@@ -1,6 +1,23 @@
 SELECT
     product_id,
-    base_price_usd,
-    cost_price_usd,
-    gross_margin_pct
+    
+    CASE 
+        WHEN base_price_usd IS NULL OR TRY_CONVERT(DECIMAL(10,2), REPLACE(REPLACE(base_price_usd, ',', ''),'$', '')) IS NULL THEN NULL
+        WHEN TRY_CONVERT(DECIMAL(10,2), REPLACE(REPLACE(base_price_usd, ',', ''),'$', '')) < 0 THEN NULL 
+        ELSE TRY_CONVERT(DECIMAL(10,2), REPLACE(REPLACE(base_price_usd, ',', ''),'$', ''))
+    END AS base_price_usd,
+
+    CASE 
+        WHEN cost_price_usd IS NULL OR TRY_CONVERT(DECIMAL(10,2), REPLACE(REPLACE(cost_price_usd, ',', ''),'$', '')) IS NULL THEN NULL
+        WHEN TRY_CONVERT(DECIMAL(10,2), REPLACE(REPLACE(cost_price_usd, ',', ''),'$', '')) < 0 THEN NULL 
+        ELSE TRY_CONVERT(DECIMAL(10,2), REPLACE(REPLACE(cost_price_usd, ',', ''),'$', ''))
+    END AS cost_price_usd,
+
+    CASE 
+        WHEN gross_margin_pct IS NULL OR TRY_CONVERT(DECIMAL(5,1), gross_margin_pct) IS NULL THEN NULL
+        WHEN TRY_CONVERT(DECIMAL(5,1), gross_margin_pct) > 100 THEN NULL 
+        ELSE TRY_CONVERT(DECIMAL(5,1), gross_margin_pct)
+    END AS gross_margin_pct
 FROM {{ ref('stg_products') }} ;
+
+
